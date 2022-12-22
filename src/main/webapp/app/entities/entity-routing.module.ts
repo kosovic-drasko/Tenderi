ca62c88a2803a99631f4d5a9944c1216b7e29b5a
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-12-22 20:57:20 +0100

    init

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,61 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'ponude',
+        data: { pageTitle: 'tenderiApp.ponude.home.title' },
+        loadChildren: () => import('./ponude/ponude.module').then(m => m.PonudeModule),
+      },
+      {
+        path: 'hvale-ponude',
+        data: { pageTitle: 'tenderiApp.hvalePonude.home.title' },
+        loadChildren: () => import('./hvale-ponude/hvale-ponude.module').then(m => m.HvalePonudeModule),
+      },
+      {
+        path: 'ponudjaci',
+        data: { pageTitle: 'tenderiApp.ponudjaci.home.title' },
+        loadChildren: () => import('./ponudjaci/ponudjaci.module').then(m => m.PonudjaciModule),
+      },
+      {
+        path: 'postupci',
+        data: { pageTitle: 'tenderiApp.postupci.home.title' },
+        loadChildren: () => import('./postupci/postupci.module').then(m => m.PostupciModule),
+      },
+      {
+        path: 'view-ponude',
+        data: { pageTitle: 'tenderiApp.viewPonude.home.title' },
+        loadChildren: () => import('./view-ponude/view-ponude.module').then(m => m.ViewPonudeModule),
+      },
+      {
+        path: 'specifikacije',
+        data: { pageTitle: 'tenderiApp.specifikacije.home.title' },
+        loadChildren: () => import('./specifikacije/specifikacije.module').then(m => m.SpecifikacijeModule),
+      },
+      {
+        path: 'tenderi-home',
+        data: { pageTitle: 'tenderiApp.tenderiHome.home.title' },
+        loadChildren: () => import('./tenderi-home/tenderi-home.module').then(m => m.TenderiHomeModule),
+      },
+      {
+        path: 'vrednovanje',
+        data: { pageTitle: 'tenderiApp.vrednovanje.home.title' },
+        loadChildren: () => import('./vrednovanje/vrednovanje.module').then(m => m.VrednovanjeModule),
+      },
+      {
+        path: 'prvorangirani',
+        data: { pageTitle: 'tenderiApp.prvorangirani.home.title' },
+        loadChildren: () => import('./prvorangirani/prvorangirani.module').then(m => m.PrvorangiraniModule),
+      },
+      {
+        path: 'view-vrednovanje',
+        data: { pageTitle: 'tenderiApp.viewVrednovanje.home.title' },
+        loadChildren: () => import('./view-vrednovanje/view-vrednovanje.module').then(m => m.ViewVrednovanjeModule),
+      },
+      {
+        path: 'view-prvorangirani',
+        data: { pageTitle: 'tenderiApp.viewPrvorangirani.home.title' },
+        loadChildren: () => import('./view-prvorangirani/view-prvorangirani.module').then(m => m.ViewPrvorangiraniModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
