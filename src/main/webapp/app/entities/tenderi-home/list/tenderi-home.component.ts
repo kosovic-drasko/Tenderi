fd036f68dda1048f300d659685e19afa4eece8d9
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-12-26 15:12:36 +0100

    init

--- src/main/webapp/app/entities/tenderi-home/list/tenderi-home.component.ts
+++ src/main/webapp/app/entities/tenderi-home/list/tenderi-home.component.ts
@@ -1,13 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Data, ParamMap, Router } from '@angular/router';
-import { combineLatest, Observable, switchMap, tap } from 'rxjs';
-
-import { ASC, DESC, SORT, DEFAULT_SORT_DATA } from 'app/config/navigation.constants';
-import { SortService } from 'app/shared/sort/sort.service';
-
 @Component({
   selector: 'jhi-tenderi-home',
   templateUrl: './tenderi-home.component.html',
+  styleUrls: ['./tenderi-home.components.scss'],
 })
 export class TenderiHomeComponent implements OnInit {
   isLoading = false;
