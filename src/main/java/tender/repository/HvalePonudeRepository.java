13522d6280d2f08202c977faa91c8df6c91413ec
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-12-27 10:02:09 +0100

    init

--- src/main/java/tender/repository/HvalePonudeRepository.java
+++ src/main/java/tender/repository/HvalePonudeRepository.java
@@ -1,5 +1,8 @@
 package tender.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 import tender.domain.HvalePonude;
